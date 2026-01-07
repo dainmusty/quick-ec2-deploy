1957be0fcb1f808ed2c46baedf1c725f54890b32
musty <dainmusty@gmail.com>
2026-01-07 11:50:08 +0000

    use this to deploy ec2 with the luxury of choosing 5-OS types

--- root/main.tf
+++ root/main.tf
@@ -86,7 +86,7 @@ module "security_group" {
 
 # Existing Iam Instance profile
 data "aws_iam_instance_profile" "admin" {
-  name = "your-admin-instance-profile-name"  # Replace with your actual instance profile name
+  name = "your-instance-profile-name"
 }
 
 
@@ -98,7 +98,7 @@ module "ec2" {
   ami_ids                    = ["ami-068c0051b15cdb816", "ami-02a53b0d62d37a757", "ami-02e3d076cbd5c28fa", "ami-0dfc569a8686b9320", "ami-04b4f1a9cf54c11d0"]
   ami_names                  = ["AL2023", "AL2", "Windows", "RedHat", "ubuntu"]
   instance_types             = ["t2.micro", "t2.micro", "t2.micro", "t3.large", "t2.micro"]
-  key_name                   = "your-key-pair-name"  # Replace with your actual key pair name
+  key_name                   = "your-key-name"
   volume_size                = 8
   volume_type                = "gp3"
 
@@ -140,12 +140,104 @@ module "ec2" {
   private_subnet_ids         = module.vpc.vpc_private_subnets
   public_sg_id               = module.security_group.public_sg_id
   private_sg_id              = module.security_group.private_sg_id
- 
+
+  
+
+
 }
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Use this version to setup ansible tower server
+# # EC2 Module
+# module "ec2" {
+#   source = "../modules/ec2"
+
+#   ResourcePrefix             = "Dev"
+#   ami_ids                    = ["ami-08982f1c5bf93d976", "ami-02a53b0d62d37a757", "ami-02e3d076cbd5c28fa", "ami-0dfc569a8686b9320", "ami-04b4f1a9cf54c11d0"]
+#   ami_names                  = ["AL2023", "AL2", "Windows", "RedHat", "ubuntu"]
+#   instance_types             = ["t2.micro", "t2.micro", "t2.micro", "t3.large", "t2.micro"]
+#   key_name                   = "us-east-1-musty"
+#   volume_size                = 50
+#   volume_type                = "gp3"
+
+#   # Pass the instance profile name from data source
+#   admin_profile_name         = data.aws_iam_instance_profile.admin.name
+
+#   public_instance_count      = [0, 0, 0, 1, 0]
+#   private_instance_count     = [0, 0, 0, 0, 0]
+
+#   # Userdata
+#   private_user_data = file("${path.module}/../scripts/private_userdata.sh") # Update the user_data script as needed
+#   public_user_data = templatefile("${path.module}/../scripts/setup_ansible_tower.sh", {
+
+#     # Pass variables to the template
+  
+#   SSM_PARAMETER_NAME         = "/ansible/tower"
+#   REDHAT_USERNAME            = "your_redhat_username or email"
+#   S3_BUCKET                  = "your s3 bucket name"
+#   TOWER_ARCHIVE              = "ansible-automation-platform-setup-bundle-2.4-1-x86_64.tar.gz"
+#   TOWER_FOLDER               = "/root/ansible-automation-platform-setup-bundle-2.4-1-x86_64"
+#   INVENTORY_FILE             = "/root/ansible-automation-platform-setup-bundle-2.4-1-x86_64/inventory"
+#   INSTALLER_SOURCE           = "s3://your-s3-bucket-name/ansible-automation-platform-setup-bundle-2.4-1-x86_64.tar.gz"
+
+#   }) # Update the user_data script as needed
+
+#   # Tags 
+#   tag_value_public_instances = [
+#     [
+#       {
+#         role        = "users"
+#         Environment = "Dev"
+#       },
+#     ],
+#     [], [], [], []
+#   ]
+
+#   tag_value_private_instances = [
+#     [],
+#     [
+#       {
+#         Name = "db1"
+#         Tier = "Database"
+#       }
+#     ],
+#     [], [], []
+#   ]
+
+#   vpc_id                     = module.vpc.vpc_id
+#   public_subnet_ids          = module.vpc.vpc_public_subnets
+#   private_subnet_ids         = module.vpc.vpc_private_subnets
+#   public_sg_id               = module.security_group.public_sg_id
+#   private_sg_id              = module.security_group.private_sg_id
+
+# }
 
 
 
